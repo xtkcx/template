c145ff50d1232bb549874ed174c66c204e0b0083
xtkcx <1117take@gmail.com>
2024-07-26 20:18:18 +0900

    DateUtil.javaの作成

--- src/com/diworksdev/template/util/DateUtil.java
+++ src/com/diworksdev/template/util/DateUtil.java
@@ -1,5 +1,12 @@
 package com.diworksdev.template.util;
 
-public class DateUtil {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class DateUtil {
+	public String getDate() {
+		Date date = new Date();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		return simpleDateFormat.format(date);
+	}
 }
